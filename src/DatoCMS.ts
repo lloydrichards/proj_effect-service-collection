17e4f2e6f53e3e77f475ea9d0ea67d0e3cae0196
lloydrichards <lloyd.d.richards@gmail.com>
2025-07-18 08:27:17 +0200

    feat: add DatoCMS service

---
+++ src/DatoCMS.ts
@@ -0,0 +1,83 @@
+import {
+  executeQuery,
+  executeQueryWithAutoPagination,
+} from "@datocms/cda-client";
+import { Config, Data, Effect, Layer } from "effect";
+
+type QueryVariables = Record<string, unknown>;
+
+class DatoFailure extends Data.TaggedError("DatoFailure")<{
+  message: string;
+  error: unknown;
+}> {}
+
+const DatoContentConfig = Config.all({
+  token: Config.string("DATOCMS_API_TOKEN"),
+  environment: Config.string("DATOCMS_API_ENV").pipe(
+    Config.withDefault("main"),
+  ),
+  includeDrafts: Config.boolean("DATOCMS_INCLUDE_DRAFTS").pipe(
+    Config.withDefault(true),
+  ),
+});
+
+export class DatoCMS extends Effect.Service<DatoCMS>()("app/DatoCMS", {
+  effect: Effect.gen(function* () {
+    const config = yield* DatoContentConfig;
+
+    const query = (
+      query: string,
+      options?: {
+        variables?: QueryVariables;
+      },
+    ) =>
+      Effect.tryPromise({
+        try: () =>
+          executeQuery(query, {
+            ...config,
+            variables: options?.variables,
+          }),
+        catch: (error) =>
+          new DatoFailure({
+            message: "Failed to execute query",
+            error,
+          }),
+      });
+
+    const queryWithPagination = (
+      query: string,
+      options?: {
+        variables?: QueryVariables;
+      },
+    ) =>
+      Effect.tryPromise({
+        try: () =>
+          executeQueryWithAutoPagination(query, {
+            ...config,
+            variables: options?.variables,
+          }),
+        catch: (error) =>
+          new DatoFailure({
+            message: "Failed to execute query with pagination",
+            error,
+          }),
+      });
+
+    return {
+      query,
+      queryWithPagination,
+    } as const;
+  }),
+  accessors: true,
+}) {}
+
+export const DatoCMSLive = DatoCMS.Default;
+
+export const DatoCMSTest = Layer.succeed(
+  DatoCMS,
+  DatoCMS.make({
+    query: () => Effect.die("Test query not implemented"),
+    queryWithPagination: () =>
+      Effect.die("Test query with pagination not implemented"),
+  }),
+);
